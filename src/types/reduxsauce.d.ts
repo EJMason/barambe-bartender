26e732bfae732acd72a1196f5d3b318e2007dc30
EJMason <eliotjunior@gmail.com>
2017-09-27 01:33:13 -0700

    [types] Added definitions for Redux and untyped packages...

---
+++ src/types/reduxsauce.d.ts
@@ -0,0 +1,11 @@
+
+
+declare module 'reduxsauce' {
+  export function createReducer (one?: any, two?: any): any
+  export function createTypes(str1?: any, str2?: any): any
+  export function createActions (one?: any, two?: any): any
+
+}
+
+// export as namespace reduxsauce;
+// export = reduxsauce;
