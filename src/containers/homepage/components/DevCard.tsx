623cc233b95414fe6e3ee4622d32c26657d47d2b
EJMason <eliotjunior@gmail.com>
2017-10-03 18:35:47 -0700

    [prog] Developer section added to homepage

---
+++ src/containers/homepage/components/DevCard.tsx
@@ -0,0 +1,75 @@
+/**
+ * Dev card container item
+ */
+
+import * as React from 'react';
+import { Button } from 'semantic-ui-react';
+// import ghLogo from '../../../assets/gh-logo.png';
+
+interface Props {
+  name: string;
+  img: string;
+  links: Links;
+}
+
+interface Links {
+  github: string;
+  linkedin: string;
+  stack: string;
+  email: string;
+}
+
+export default (props: Props) => {
+  return (
+    <div className="dev-card">
+      <div id="card-body" className="">
+        <div className="card-text">
+          <h1>{props.name}</h1>
+          <p>Full Stack Developer</p>
+          <div className="logos-ctr">
+            <Button
+              as="a"
+              href={props.links.github}
+              target="_blank"
+              circular
+              color="black"
+              icon="github alternate"
+            />
+            <Button
+              as="a"
+              href={props.links.linkedin}
+              target="_blank"
+              circular
+              color="linkedin"
+              icon="linkedin"
+            />
+            <Button
+              as="a"
+              href={props.links.stack}
+              target="_blank"
+              circular
+              color="orange"
+              icon="stack overflow"
+            />
+            <Button circular color="google plus" icon="mail" />
+          </div>
+          <div className="button-ctr">
+            <Button
+              basic
+              color="black"
+              fluid
+              content="Learn More"
+              icon="right arrow"
+              labelPosition="right"
+            />
+          </div>
+        </div>
+      </div>
+      <img
+        id="prflimg"
+        src={props.img}
+        alt="profile img"
+      />
+    </div>
+  );
+};
---
+++ src/containers/homepage/components/developers.tsx
@@ -0,0 +1,30 @@
+/**
+ * Banner container item
+ */
+import '../../../styles/main.css';
+import * as React from 'react';
+import {
+  Divider,
+  Icon } from 'semantic-ui-react';
+import DevCard from './DevCard';
+import { developers } from '../../../assets';
+
+export default () => {
+  return (
+    <div id="devs-container" className="">
+      <h3>LEARN ABOUT THE DEVELOPERS</h3>
+      <hr className="hr-divider" />
+      <div className="dev-cards-ctr">
+        {developers.map((props, i) => <DevCard {...props} key={`${i}_devs`} />)}
+      </div>
+      <Divider hidden />
+      <div className="devs-more-ctr">
+        <div>
+          <p>Learn more about how we built the app</p>
+        </div>
+        <Icon inverted circular color="yellow" name="right arrow" />
+      </div>
+      <Divider section hidden />
+    </div>
+  );
+};
