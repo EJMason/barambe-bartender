b083cf4b0203dda1ce547fc6feb2df9bdc8c0185
EJMason <eliotjunior@gmail.com>
2017-10-03 20:07:43 -0700

    [prog] minor content change, button size

--- src/containers/homepage/components/bartenderApp.tsx
+++ src/containers/homepage/components/bartenderApp.tsx
@@ -8,13 +8,13 @@ import { Divider, Header, Button } from 'semantic-ui-react';
 export default () => {
   return (
     <div className="bartender-ctr">
-      <h2 className="home-header-grey">Bartender Web Application</h2>
+      <h2 className="home-header-grey">BARTENDER CLIENT APPLICATION</h2>
       <Divider hidden />
       <p className="">
         {'We want to make life easier for the bartenders. \
         Barambe takes care of all the little stuff that takes \
-        up precious time so you canfocus on the good parts about \
-        being a bartender. We know those tips pay the bills. \
+        up precious time so you can focus on the good parts about \
+        your job. We know those tips pay the bills. \
         Let\'s get your more of them.'}
       </p>
       <div className="bartender-items">
@@ -30,7 +30,7 @@ export default () => {
       <p>{'Owners love Barambe too! \
         Be the bar that people talk about.'}</p>
       <div className="btn-ctr">
-        <Button basic fluid color="blue">
+        <Button basic fluid size="large" color="blue">
           Get your bar signed up today!
         </Button>
       </div>
